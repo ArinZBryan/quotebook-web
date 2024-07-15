ded3b1bcd924ee519c2b695f712e82c4114df6f0
ArinZBryan <59438587+ArinZBryan@users.noreply.github.com>
2024-07-15 19:15:56 +0100

    Added DEV_REDIRECT evironment variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-
+import { redirect } from 'next/navigation'
 import { Login } from '@/components/component/login'
 import { ShowOnLogin } from '@/components/component/showonlogin'
 import { TitleBar } from '@/components/component/titlebar'
 import { SessionProvider, useSession } from 'next-auth/react'
 import { Dashboard } from './dashboard/dashboard'
 
+
 export default function Home() {
+
+    if (process.env.DEV_REDIRECT != "") {
+        redirect(process.env.DEV_REDIRECT!)
+    }
 
     return (<div>
         <SessionProvider>
